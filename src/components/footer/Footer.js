e18b7b42a4833eb34afd57b63391f9f7b29b616a
Yurii_Yevchenko <yurievch@gmail.com>
2024-02-19 14:22:39 +0200

    загрузка проекта

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,44 @@
+import './style.css';
+
+import instagram from './../../img/icons/instagram.svg';
+import twitter from './../../img/icons/twitter.svg';
+import linkedIn from './../../img/icons/linkedIn.svg';
+import gitHub from './../../img/icons/gitHub.svg';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="footer__wrapper">
+          <ul className="social">
+            <li className="social__item">
+              <a href="#!">
+                <img src={instagram} alt="Link" />
+              </a>
+            </li>
+            <li className="social__item">
+              <a href="#!">
+                <img src={twitter} alt="Link" />
+              </a>
+            </li>
+            <li className="social__item">
+              <a href="#!">
+                <img src={gitHub} alt="Link" />
+              </a>
+            </li>
+            <li className="social__item">
+              <a href="#!">
+                <img src={linkedIn} alt="Link" />
+              </a>
+            </li>
+          </ul>
+          <div className="copyright">
+            <p>© 2024 work.yevchenko@gmail.com</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/pages/Home.js
@@ -0,0 +1,25 @@
+import Header from './../components/header/Header';
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <main className="section">
+        <div className="container">
+          <ul className="content-list">
+            <li className="content-list__item">
+              <h2 className="title-2">Frontend</h2>
+              <p>HTML, CSS, Figma, CSS Grid, Git, VS Code</p>
+            </li>
+            <li className="content-list__item">
+              <h2 className="title-2">Backend</h2>
+              <p>JavaScript, ReactJS</p>
+            </li>
+          </ul>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Home;
